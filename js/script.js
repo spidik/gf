f5c85b433971d1a1d829fc10ad6b54820ca97c46
ILYA <orionmir.sk@bk.ru>
2022-08-16 13:10:22 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+console.log('arr' + ' - obj');
+console.log(4 + +'5');
+
+let incr = 10,
+  decr = 10;
+
+incr++;
+decr--;
+console.log(++incr);
+console.log(--decr);
+
+console.log(5 % 2);
+
+console.log(2 + 32);
